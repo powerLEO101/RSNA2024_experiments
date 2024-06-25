680a9988e6fdb4d14d3d958cc789cc7fa2055e69
Leo <powerleo101@gmail.com>
2024-06-26 02:02:35 +0800

    run autodl

---
+++ run_autodl.py
@@ -0,0 +1,31 @@
+jobs = ['python t001_from000_baseline.py']
+
+import os
+import requests
+import subprocess
+os.environ['HF_HOME'] = '/root/autodl-tmp/cache/'
+
+for job in jobs:
+    msg_sent = 0
+    try:
+        subprocess.check_call(job, shell=True)
+    except:
+        print('!!!!!!!!!!!!!Failed!!!!!!!!!!!!')
+        headers = {"Authorization": "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjQyNjMwMCwidXVpZCI6ImNjYmRmZWYyLWI4ODQtNDM5YS1hNmM2LThjM2M2MGNiY2FmMiIsImlzX2FkbWluIjpmYWxzZSwiYmFja3N0YWdlX3JvbGUiOiIiLCJpc19zdXBlcl9hZG1pbiI6ZmFsc2UsInN1Yl9uYW1lIjoiIiwidGVuYW50IjoiYXV0b2RsIiwidXBrIjoiIn0.JCFY0Rt9JWpRFDIOzlY86V7jhKZFCvJ2Hrjy0HVKhw3v1ULOsNhAhVhvEzyYUEkxZ_lhgYYHd6LpGEmTRi-4BA"}
+        resp = requests.post("https://www.autodl.com/api/v1/wechat/message/send",
+                            json={
+                                "title": "RSNA实验失败",
+                                "name": "RSNA实验失败",
+                                "content": "bad"
+                            }, headers = headers)
+        print(resp.content.decode())
+        msg_sent = 1
+    if msg_sent == 0:
+        headers = {"Authorization": "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjQyNjMwMCwidXVpZCI6ImNjYmRmZWYyLWI4ODQtNDM5YS1hNmM2LThjM2M2MGNiY2FmMiIsImlzX2FkbWluIjpmYWxzZSwiYmFja3N0YWdlX3JvbGUiOiIiLCJpc19zdXBlcl9hZG1pbiI6ZmFsc2UsInN1Yl9uYW1lIjoiIiwidGVuYW50IjoiYXV0b2RsIiwidXBrIjoiIn0.JCFY0Rt9JWpRFDIOzlY86V7jhKZFCvJ2Hrjy0HVKhw3v1ULOsNhAhVhvEzyYUEkxZ_lhgYYHd6LpGEmTRi-4BA"}
+        resp = requests.post("https://www.autodl.com/api/v1/wechat/message/send",
+                            json={
+                                "title": "RSNA实验成功",
+                                "name": "RSNA实验成功",
+                                "content": "good"
+                            }, headers = headers)
+        print(resp.content.decode())
