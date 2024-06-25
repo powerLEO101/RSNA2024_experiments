fae5c6c936e79d007744e6c48370a162cb2af909
Leo <powerleo101@gmail.com>
2024-06-26 02:01:57 +0800

    change

--- core/project_paths.py
+++ core/project_paths.py
@@ -1,2 +1,2 @@
-base_path = '/autodl-tmp/input/rsna-2024-lumbar-spine-degenerative-classification'
-save_path = '/autodl-tmp/checkpoints'
+base_path = '/root/autodl-tmp/input/rsna-2024-lumbar-spine-degenerative-classification'
+save_path = '/root/autodl-tmp/checkpoints'
