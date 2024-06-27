6d7bd902d07a868c96ca48cd7530f2bfe7cf661c
Leo <powerleo101@gmail.com>
2024-06-27 22:41:00 +0800

    t002 more epoch

--- core/models.py
+++ core/models.py
@@ -3,12 +3,12 @@ import torch
 import torch.nn as nn
 
 class BaselineModel(nn.Module):
-    def __init__(self, model_name):
+    def __init__(self, model_name, in_chans=6):
         super().__init__()
         self.model = timm.create_model(model_name=model_name,
                                        pretrained=True,
                                        num_classes=25 * 3,
-                                       in_chans=6)
+                                       in_chans=in_chans)
     def forward(self, x):
         x = self.model(x)
         return x
