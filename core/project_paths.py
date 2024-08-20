63599e0756b2350dd667e80a7fe1cd005adcce8e
Leo <powerleo101@gmail.com>
2024-08-20 13:07:04 -0400

    debug

--- core/project_paths.py
+++ core/project_paths.py
@@ -4,5 +4,5 @@ if 'LOCAL_TEST' in environ:
     base_path = '/Users/leo101/Kaggle/rsna-2024-lumbar-spine-degenerative-classification/input/rsna-2024-lumbar-spine-degenerative-classification'
     save_path = '/Users/leo101/Kaggle/rsna-2024-lumbar-spine-degenerative-classification/input/RSNA24_checkpoints'
 else:
-    base_path = '/root/autodl-tmp/input/rsna-2024-lumbar-spine-degenerative-classification'
-    save_path = '/root/autodl-fs'
+    base_path = '/media/workspace/RSNA2024_input/rsna-2024-lumbar-spine-degenerative-classification'
+    save_path = '/media/workspace/RSNA2024_checkpoints'
