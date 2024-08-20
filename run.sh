84b66378f661afcf20f54781a4f23d606ddf73ab
Leo <powerleo101@gmail.com>
2024-08-20 11:11:58 -0400

    t043

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git pull
-source /etc/network_turbo
-source /root/autodl-tmp/RSNA/bin/activate
-python run_autodl.py
+git add -A .
+git commit -m "$1"
+git push origin main
+ssh 100.82.29.87 -t 'cd /media/workspace/RSNA2024_experiments; git pull; cat jobs.txt >> ../jobs.txt; exit'
